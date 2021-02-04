2de46b8904326a2e5c6cb3e416ac1029e315a68c
Kat Wilson <katjanewilson@gmail.com>
2021-02-04 13:46:00 +0000

    edits

---
+++ removed.R
@@ -0,0 +1,64 @@
+#### removed from index
+
+
+
+#### Philadelphia Public Schools
+
+
+
+<div class="row">
+  <div class="col-sm-6">
+    
+    |Overview |  |
+    |:--------|-------:|
+    
+    In experiments where randomization is not feasible, propensity score matching helps to control for confounded relationships among variables. Analysts working in this causal framework often run into a particular issue: sample size affects their ability to arrive at evenly matched samples. The problem is especially prevalent in observational studies that use administrative data. In partnernship with the Yale National Initiative, we evaluate trends in teacher retention across the Philadelphia School District, and arrive at better percent balance improvement in our causal model by trimming mis-represented groups. </br> *data private on github
+  
+  |Technical |  |
+    |:--------|-------:|
+    Data cleaning: tidyverse </br>
+    Causal Inference: R (MatchIt)
+  
+  
+  <a href="https://github.com/katjanewilson/Yale-National-Initiative" target = "_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+    
+    
+    </div>
+    <div class="col-sm-6">
+      
+      <a href="https://katjanewilson.github.io/Yale-National-Initiative/"  target = "_blank">
+        ```{r, out.width=300, out.height=200, echo=F}
+      knitr::include_graphics("images/qq.png")
+      ```
+      </a>
+        
+        </div>
+        </div>
+        
+        
+        ---
+        
+        
+        
+        
+        
+        #### Growth Mindset Intervention
+        
+        
+        <div class="row">
+        <div class="col-sm-6">
+        
+        |Overview |  |
+        |:--------|-------:|
+        
+        Dual Enrollment programs offer high school students the opportunity to explore college curricula and earn college credit before graduation. With a large number of students participating in these programs each year in the state of Texas, researchers have the comfort of a large sample size and the opportunity to ethically randomize interventions. We model course completion rates for over 3,000 pre-calculus students and use natural language processing to build a survey tool that better predicts levels of growth mindset among pre-college students.
+      
+      |Technical |  |
+        |:--------|-------:|
+        
+        Text Analysis: AFINN sentiment analysis </br>
+        Linear Mixed-Effects Models: lme4
+      
+      
+      </div>
+        </div>
